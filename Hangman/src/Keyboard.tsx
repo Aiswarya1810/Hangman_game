1de6373057a8258dd619a70034edef6fa9d485aa
Aiswarya1810 <s4070432@student.rmit.edu.au>
2026-04-11 11:28:01 +1000

    keyboard

---
+++ Hangman/src/Keyboard.tsx
@@ -0,0 +1,32 @@
+import styles from "./Keyboard.module.css"
+
+const KEYS = [
+  "a","b","c","d","e","f","g","h","i","j",
+  "k","l","m","n","o","p","q","r","s","t",
+  "u","v","w","x","y","z"
+]
+
+export function Keyboard(){
+    return (
+    <div 
+    style={{
+        display: "grid",
+        gridTemplateColumns:"repeat(auto-fit,minmax(75px,1fr))",
+        gap:".5rem",
+        width:"100%"
+
+    }}>
+        {KEYS.map(key => {
+            return(
+                <button className = {`${styles.btn} `} 
+                key={key}>
+                    {key}
+                    </button>
+            )
+        })}
+
+
+        </div>
+    )
+
+}
