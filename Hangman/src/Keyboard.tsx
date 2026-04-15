7c84eb065430e85365bcbe6ecd41c067abde6fdc
Aiswarya1810 <s4070432@student.rmit.edu.au>
2026-04-16 00:15:06 +1000

    keyboard

--- Hangman/src/Keyboard.tsx
+++ Hangman/src/Keyboard.tsx
@@ -6,8 +6,20 @@ const KEYS = [
   "u","v","w","x","y","z"
 ]
 
-export function Keyboard(){
-    return (
+type KeyboardProps={
+    disabled: boolean
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter:(letter:string) => void
+}
+export function Keyboard({
+    activeLetters,
+    disabled = false,
+    inactiveLetters,
+    addGuessedLetter }: 
+    KeyboardProps ){
+   
+        return (
     <div 
     style={{
         display: "grid",
@@ -17,8 +29,14 @@ export function Keyboard(){
 
     }}>
         {KEYS.map(key => {
+            const isActive =  activeLetters.includes(key)
+            const isInactive= inactiveLetters.includes(key)
             return(
-                <button className = {`${styles.btn} `} 
+                <button 
+                onClick={() => addGuessedLetter(key)}
+                className = {`${styles.btn} ${isActive ? styles.active:""}
+                ${isInactive ? styles.inactive:""}`} 
+                disabled={isInactive|| isActive|| disabled}
                 key={key}>
                     {key}
                     </button>
